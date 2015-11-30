8ba17d7b73200f5b555269ce7d7dcc66e343aa12
zhengying <zy840055899@163.com>
2015-11-30 16:49:52 +0800

    尝试结合quartz,httpclient,nginx完成jsp页面静态化(商城必备技能)

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -4,15 +4,10 @@ import com.test.dao.TestDao;
 import com.test.entity.TestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.util.List;
 
 /**
