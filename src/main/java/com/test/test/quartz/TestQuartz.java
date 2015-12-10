4b80004a65423b2594f20260ef0761741af0c7ca
zhengying <zy840055899@163.com>
2015-12-10 14:53:21 +0800

    静态化案例完成

--- src/main/java/com/test/test/quartz/TestQuartz.java
+++ src/main/java/com/test/test/quartz/TestQuartz.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**
