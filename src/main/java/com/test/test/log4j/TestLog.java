fd6f74a3c65e29cad04be45711c512b2ed07ebae
zhengying <zy840055899@163.com>
2015-12-10 19:07:14 +0800

    slf4j-log4j的使用

---
+++ src/main/java/com/test/test/log4j/TestLog.java
@@ -0,0 +1,28 @@
+package com.test.test.log4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by zhengying on 15/12/10.
+ */
+public class TestLog {
+
+    static Logger logger = LoggerFactory.getLogger(TestLog.class);
+
+    public static void main(String[] args) throws IOException {
+
+        // 记录error信息
+        logger.error("[info message]");
+        // 记录info，还可以传入参数
+        logger.info("[info message]{},{},{},{}", "abc", false, 123, new TestLog());
+        // 记录deubg信息
+        logger.debug("[debug message]");
+        // 记录trace信息
+        logger.trace("[trace message]");
+        logger.debug("asdasda");
+    }
+
+}
