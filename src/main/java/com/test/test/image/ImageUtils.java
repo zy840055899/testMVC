efbee31b4f921e4343576025e84db4fcfc5b395a
zhengying <zy840055899@163.com>
2016-01-14 10:23:28 +0800

    图片处理类

--- src/main/java/com/test/test/image/ImageUtils.java
+++ src/main/java/com/test/test/image/ImageUtils.java
@@ -23,6 +23,8 @@ import javax.imageio.ImageIO;
 /**
  * 图片处理工具类：<br>
  * 功能：缩放图像、切割图像、图像类型转换、彩色转黑白、文字水印、图片水印等
+ *
+ * 原文:http://www.cnblogs.com/liyunqi007/archive/2011/11/23/2260052.html
  */
 public class ImageUtils {
 
